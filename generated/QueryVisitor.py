65c6992b2e916869964b5ecefb2500ef1102eff6
GMalho <mgorav@gmail.com>
2020-10-11 13:22:17 +0200

    refactoring

--- generated/QueryVisitor.py
+++ generated/QueryVisitor.py
@@ -3,7 +3,7 @@ from antlr4 import *
 if __name__ is not None and "." in __name__:
     from .QueryParser import QueryParser
 else:
-    from RsqlParser import RsqlParser
+    from .QueryParser import QueryParser
 
 # This class defines a complete generic visitor for a parse tree produced by QueryParser.
 
